554811dd12006011565f054aa66276517d41f6fa
MohBrkat <mbarakat@itgsoftware.com>
2020-10-26 10:13:25 +0300

    cont.

--- SyncApp/Models/FileInformation.cs
+++ SyncApp/Models/FileInformation.cs
@@ -12,8 +12,8 @@ namespace SyncApp.Models
         public bool isValid { get; set; }
         public int lsErrorCount { get; set; }
         public int lsSuccessCount { get; set; }
-        public List<string> LsOfErrors { set; get; }
-        public List<string> LsOfSucess { set; get; }
+        public List<string> LsOfErrors { set; get; } = new List<string>();
+        public List<string> LsOfSucess { set; get; } = new List<string>();
         public List<string> fileRows { set; get; }
         public IFormFile File { set; get; }
 
