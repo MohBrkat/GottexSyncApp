a4fb7eaa8af5edffeaf44855125d224130c43b50
MohBrkat <mbarakat@itgsoftware.com>
2020-11-18 14:22:56 +0200

    cont.

--- SyncApp/Controllers/HomeController.cs
+++ SyncApp/Controllers/HomeController.cs
@@ -907,20 +907,6 @@ namespace ShopifyApp2.Controllers
         #endregion
 
         #region General Functions
-        private static string[] RemoveEmptyLastRow(string[] Rows)
-        {
-            var LastRow = Rows.LastOrDefault();
-            if (LastRow.IsNotNullOrEmpty())
-            {
-                if (!LastRow.Split(",")[0].IsNotNullOrEmpty() && !LastRow.Split(",")[1].IsNotNullOrEmpty() && !LastRow.Split(",")[2].IsNotNullOrEmpty() && !LastRow.Split(",")[3].IsNotNullOrEmpty())
-                {
-                    Rows.ToList().RemoveAt(Rows.Count() - 1);
-                    Rows = Rows.ToArray();
-                }
-            }
-
-            return Rows;
-        }
         private bool IsValidHeaders(string Headers)
         {//product handle,
             var arr = Headers.Trim().ToLower().Split(",");
