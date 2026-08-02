0a0849db1a9c51a1a9095b9343b63e32b09162b2
DuaaBraik <dbraik@itgsoftware.com>
2026-08-02 12:07:47 +0300

    Fixed lineItems page size to 50

--- SyncApp/Helpers/ShopifyGraphQlHelper.cs
+++ SyncApp/Helpers/ShopifyGraphQlHelper.cs
@@ -89,7 +89,7 @@ namespace SyncAppCommon.Helpers
                     displayFinancialStatus
                     displayFulfillmentStatus
                     tags
-                    lineItems(first: 100) {
+                    lineItems(first: 50) {
                       nodes {
                         id
                         quantity
@@ -157,7 +157,7 @@ namespace SyncAppCommon.Helpers
                           }
                         }
                       }
-                      refundLineItems(first: 100) {
+                      refundLineItems(first: 50) {
                         nodes {
                           quantity
                           restockType
